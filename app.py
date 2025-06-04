7bc832a989d91202441b7862acacc3362f62961c
Vamshi Krishna Adam <46240162+vams2krish@users.noreply.github.com>
2025-06-04 14:38:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,24 +6,26 @@ import platform
 if platform.system() == 'Windows':
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-#file uploader in the streamlit UI
-file_upload = st.file_uploader("Upload a image file")
+
 
 if file_upload is not None:
-    #return  streamlit file upload status
+    #file uploader in the streamlit UI
+    file_upload = st.file_uploader("Upload a image file")
+
+    #return  streamlit file upload status   
     st.success("Image is uploaded successfully", icon='✅')
-
-#image file uploaded to PILLOW
-image = Image.open(file_upload)
-
-#Convert to grayscale image
-file = image.convert('L')
-
-#open upload image
-st.write(file)
-
-#extract text from the file upload
-extracted_text = pytesseract.image_to_string(file)
-
-#Display output text
-st.write("Extracted text:\n\n",extracted_text)
+    
+    #image file uploaded to PILLOW
+    image = Image.open(file_upload)
+    
+    #Convert to grayscale image
+    file = image.convert('L')
+    
+    #open upload image
+    st.write(file)
+    
+    #extract text from the file upload
+    extracted_text = pytesseract.image_to_string(file)
+    
+    #Display output text
+    st.write("Extracted text:\n\n",extracted_text)
